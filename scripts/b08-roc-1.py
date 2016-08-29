f3b611c0376618c2a88ee23ece958b03c64a767d
Nikolaos Perrakis <np4g15@soton.ac.uk>
2016-08-29 23:36:04 +0100

    created presentation

--- scripts/b08-roc-1.py
+++ scripts/b08-roc-1.py
@@ -10,43 +10,43 @@ import matplotlib.pyplot as plt
 
 # sq eucl - adduser
 
-acc1 = np.array([0.912087912088, 0.868131868132, 0.681318681319,
+acc1 = np.array([1, 0.912087912088, 0.868131868132, 0.681318681319,
     0.571428571429, 0.252747252747, 0.120879120879, 0.0769230769231,
-    0.0549450549451, 0.043956043956,  0.032967032967])
-fpr1 = np.array([0.775617566331, 0.394098810613, 0.25571820677,
+    0.0549450549451, 0.043956043956,  0.032967032967, 0])
+fpr1 = np.array([1, 0.775617566331, 0.394098810613, 0.25571820677,
     0.180695333943, 0.121912168344, 0.077538883806, 0.0583257090576,
-    0.0365965233303, 0.0292772186642, 0.0267612076853])
+    0.0365965233303, 0.0292772186642, 0.0267612076853, 0])
 
 # sq eucl - hydra ftp
 
-acc2 = np.array([0.728395061728, 0.635802469136, 0.561728395062,
-    0.364197530864, 0.191358024691, 0.0246913580247, 0, 0, 0, 0])
+acc2 = np.array([1, 0.728395061728, 0.635802469136, 0.561728395062,
+    0.364197530864, 0.191358024691, 0.0246913580247, 0, 0, 0, 0, 0])
 # fpr2 = fpr1
 
 # sq eucl - hydra ssh
 
-acc3 = np.array([0.795454545455, 0.477272727273, 0.409090909091,
+acc3 = np.array([1, 0.795454545455, 0.477272727273, 0.409090909091,
     0.25, 0.181818181818, 0.0625, 0.0170454545455, 0.00568181818182,
-    0.00568181818182, 0])
+    0.00568181818182, 0, 0])
 
 
 # sq eucl - java meterpreter
 
-acc4 = np.array([0.887096774194, 0.798387096774, 0.620967741935,
+acc4 = np.array([1, 0.887096774194, 0.798387096774, 0.620967741935,
     0.508064516129, 0.395161290323, 0.258064516129, 0.241935483871,
-    0.193548387097, 0.161290322581, 0.0967741935484])
+    0.193548387097, 0.161290322581, 0.0967741935484, 0])
 
 # sq eucl - meterpreter
 
-acc5 = np.array([0.866666666667, 0.8, 0.733333333333, 0.52, 0.24, 0.04,
-    0.0133333333333, 0.0133333333333, 0.0133333333333, 0])
+acc5 = np.array([1, 0.866666666667, 0.8, 0.733333333333, 0.52, 0.24, 0.04,
+    0.0133333333333, 0.0133333333333, 0.0133333333333, 0, 0])
 
 
 # sq eucl - web shell
 
-acc6 = np.array([0.957627118644, 0.85593220339, 0.593220338983,
+acc6 = np.array([1, 0.957627118644, 0.85593220339, 0.593220338983,
     0.432203389831, 0.21186440678, 0.0932203389831, 0.0762711864407,
-    0.0677966101695, 0.0677966101695, 0.0677966101695])
+    0.0677966101695, 0.0677966101695, 0.0677966101695, 0])
 
 
 # random classifier
@@ -88,13 +88,4 @@ print("Area under the curve for hydra ssh attack        is: {}".format(area3))
 print("Area under the curve for java meterpreter attack is: {}".format(area4))
 print("Area under the curve for meterpreter attack      is: {}".format(area5))
 print("Area under the curve for web shell attack        is: {}".format(area6))
-print("Area under the curve for random selection        is: {}".format(arear))
-
-# Normalizing area under roc with 1/(2*arear)
-
-print("Area under the curve for adduser attack          is: {}".format(area1/(2*arear)))
-print("Area under the curve for hydra ftp attack        is: {}".format(area2/(2*arear)))
-print("Area under the curve for hydra ssh attack        is: {}".format(area3/(2*arear)))
-print("Area under the curve for java meterpreter attack is: {}".format(area4/(2*arear)))
-print("Area under the curve for meterpreter attack      is: {}".format(area5/(2*arear)))
-print("Area under the curve for web shell attack        is: {}".format(area6/(2*arear)))
+#print("Area under the curve for random selection        is: {}".format(arear))
--- scripts/b08-roc-2.py
+++ scripts/b08-roc-2.py
@@ -9,42 +9,42 @@ import matplotlib.pyplot as plt
 
 # sq sn eucl - adduser
 
-acc1 = np.array([0.912087912088, 0.67032967033,  0.56043956044,
+acc1 = np.array([1, 0.912087912088, 0.67032967033,  0.56043956044,
     0.120879120879, 0.0549450549451, 0.032967032967, 0.021978021978,
-    0.021978021978,  0.021978021978, 0.010989010989])
-fpr1 = np.array([0.75571820677,  0.308325709058, 0.189387008234,
+    0.021978021978,  0.021978021978, 0.010989010989, 0])
+fpr1 = np.array([1, 0.75571820677,  0.308325709058, 0.189387008234,
     0.102927721866, 0.0574107959744, 0.0315645013724, 0.0180695333943,
-    0.0176120768527, 0.0176120768527, 0.0173833485819])
+    0.0176120768527, 0.0176120768527, 0.0173833485819, 0])
 
 # sq sn eucl - hydra ftp
 
-acc2 = np.array([0.703703703704, 0.58024691358, 0.351851851852,
-    0.0987654320988, 0.0493827160494, 0.00617283950617, 0, 0, 0, 0])
+acc2 = np.array([1, 0.703703703704, 0.58024691358, 0.351851851852,
+    0.0987654320988, 0.0493827160494, 0.00617283950617, 0, 0, 0, 0, 0])
 # fpr2 = fpr1
 
 # sq sn eucl - hydra ssh
 
-acc3 = np.array([0.721590909091, 0.392045454545, 0.267045454545,
+acc3 = np.array([1, 0.721590909091, 0.392045454545, 0.267045454545,
     0.0795454545455, 0.0568181818182, 0.0454545454545, 0.0113636363636,
-    0.0113636363636, 0, 0])
+    0.0113636363636, 0, 0, 0])
 
 
 # sq sn eucl - java meterpreter
 
-acc4 = np.array([0.846774193548, 0.645161290323, 0.564516129032,
+acc4 = np.array([1, 0.846774193548, 0.645161290323, 0.564516129032,
     0.258064516129, 0.25, 0.225806451613, 0.225806451613, 0.217741935484,
-    0.209677419355, 0.193548387097])
+    0.209677419355, 0.193548387097, 0])
 
 # sq sn eucl - meterpreter
 
-acc5 = np.array([0.853333333333, 0.76, 0.613333333333, 0.0933333333333,
-    0.0666666666667, 0.0266666666667, 0, 0, 0, 0])
+acc5 = np.array([1, 0.853333333333, 0.76, 0.613333333333, 0.0933333333333,
+    0.0666666666667, 0.0266666666667, 0, 0, 0, 0, 0])
 
 
 # sq sn eucl - web shell
 
-acc6 = np.array([0.957627118644, 0.661016949153, 0.457627118644,
-    0.118644067797, 0.0508474576271, 0.0169491525424, 0, 0, 0, 0])
+acc6 = np.array([1, 0.957627118644, 0.661016949153, 0.457627118644,
+    0.118644067797, 0.0508474576271, 0.0169491525424, 0, 0, 0, 0, 0])
 
 
 # random classifier
@@ -87,12 +87,3 @@ print("Area under the curve for java meterpreter attack is: {}".format(area4))
 print("Area under the curve for meterpreter attack      is: {}".format(area5))
 print("Area under the curve for web shell attack        is: {}".format(area6))
 print("Area under the curve for random selection        is: {}".format(arear))
-
-# Normalizing area under roc with 1/(2*arear)
-
-print("Area under the curve for adduser attack          is: {}".format(area1/(2*arear)))
-print("Area under the curve for hydra ftp attack        is: {}".format(area2/(2*arear)))
-print("Area under the curve for hydra ssh attack        is: {}".format(area3/(2*arear)))
-print("Area under the curve for java meterpreter attack is: {}".format(area4/(2*arear)))
-print("Area under the curve for meterpreter attack      is: {}".format(area5/(2*arear)))
-print("Area under the curve for web shell attack        is: {}".format(area6/(2*arear)))
